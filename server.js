8a14d6210f989b56f7ff3ec99d8513673ebf2d85
Mario Quintana <pyrros2000@gmail.com>
2018-06-10 12:26:44 -0700

    Created our tsam database, then created a birds table, and then created the users table. Both tables are empty but are listed in our tsam database

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.get('/', function(request, response){
     response.sendFile('./public/index.html');
 });
 
+// app.post('/articles', bodyParser, function(request, response) {
+//     // REVIEW: This route will receive a new article from the form page, new.html,
+//     // and log that form data to the console. We will wire this up soon to actually
+//     // write a record to our persistence layer!
+//     console.log(request.body);
+//     response.send('Record posted to server!!');
+//   })
+
 app.listen(PORT, function() {
     console.log(`Listening on port: ${PORT}`);
 });
