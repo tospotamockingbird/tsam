4c835826611dc47b164e9b129014915b413f048e
Mario Quintana <pyrros2000@gmail.com>
2018-06-10 10:54:25 -0700

    Created simple node server, added express and body-parser, package.json is set up.

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser').urlencoded({extended: true});
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public') );
+
+app.get('/', function(request, response){
+    response.sendFile('./public/index.html');
+});
+
+app.listen(PORT, function() {
+    console.log(`Listening on port: ${PORT}`);
+});
