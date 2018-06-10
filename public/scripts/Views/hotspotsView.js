4e24e3de93d6e200a92d0e42c3ccd56b7a307359
Taylor Morris <taylor.morris@gmail.com>
2018-06-10 16:31:50 -0700

    added some text explaining the page, made a few tweaks to the style of info windows, added a drag-to-hide event handlers on markers

--- public/scripts/Views/hotspotsView.js
+++ public/scripts/Views/hotspotsView.js
@@ -1,9 +1,9 @@
-// Initialize and add the map
+// Initialize and add the map, then add sighting markers when the ajax calls are both complete
 function initMap() {
   var portland = {lat: 45.5122, lng: -122.6587};
   var map = new google.maps.Map(
       document.getElementById('map'), {
-        zoom: 9,
+        zoom: 10,
         center: portland,
         mapTypeId: 'terrain',
         styles: [
@@ -109,26 +109,33 @@ function initMap() {
       ]
     });
 
+// when both ajax calls are complete
     $.when(
       $.ajax(oregon),
       $.ajax(washington)
     )
     .done( (oregon, washington) => {
+// concat the two arrays and assign the new array to orWA
       let orWA = oregon[0].concat(washington[0]);
+// for each object, create a location, drop a marker, create infowindow and its content, assign click handlers
       orWA.forEach( bird => {
         let location = {lat: bird.lat, lng: bird.lng};
         let marker = new google.maps.Marker({position: location, map: map});
-        let contentString = `<div class="info-window"><h1>${bird.comName}</h1><strong>Location: </strong>${bird.locName}<br><strong>Date: </strong>${bird.obsDt}<br><strong>Count: </strong>${bird.howMany}`;
+        let contentString = `<div class="info-window"><h4>${bird.comName}</h4><strong>Location: </strong>${bird.locName}<br><strong>Date: </strong>${bird.obsDt}<br><strong>Count: </strong>${bird.howMany}`;
         let infowindow = new google.maps.InfoWindow({
           content: contentString
         });
         marker.addListener('click', function() {
-          infowindow.open(map, marker);
-        });
-      })
+            infowindow.open(map, marker);
+          });
+        map.addListener('dragend', function() {
+              infowindow.close();
+            });
+        })
     });
 };
 
+// production-only
 var GMAPS_KEY = config.GMAPS_KEY
 const scriptMaker = () => {
   let gmapScript = "<script async defer src='https://maps.googleapis.com/maps/api/js?key=";
